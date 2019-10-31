2c6432479f93e4b13b224f0802a8e4fde7f5050c
melanie-fuchs <mele.fuchs@gmx.ch>
2019-10-31 13:44:14 +0100

    added emptyborder to MemorySettingPanel

--- src/gui/MemorySettingPanel.java
+++ src/gui/MemorySettingPanel.java
@@ -3,12 +3,14 @@
  */
 package gui;
 
+import java.awt.BorderLayout;
 import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
 
 /**
  * Class that handles the settings within a started memory-game.
@@ -26,6 +28,7 @@ public class MemorySettingPanel extends JPanel {
 	
 	public MemorySettingPanel() {
 		this.setLayout(new GridLayout(2, 3, 60, 5));
+		this.setBorder(new EmptyBorder(10, 10, 15, 10));
 		jbEndGame = new JButton("End Game");
 		jlAttemptsText= new JLabel("Attempts: ", SwingConstants.CENTER);
 		jlAttemptsCounter = new JLabel(attempts, SwingConstants.CENTER);
