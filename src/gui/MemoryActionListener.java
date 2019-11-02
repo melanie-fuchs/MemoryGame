376c73e8b3eb90573f3e69e910bd674247d32ad6
melanie-fuchs <mele.fuchs@gmx.ch>
2019-11-02 20:39:18 +0100

    actionListener update

--- src/gui/MemoryActionListener.java
+++ src/gui/MemoryActionListener.java
@@ -5,6 +5,10 @@ package gui;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+import javax.swing.JLabel;
 
 /**
  * @author yume
@@ -12,18 +16,27 @@ import java.awt.event.ActionListener;
  */
 public class MemoryActionListener implements ActionListener {
 
-	MemoryModel model;
+	Hashtable<Integer, BaseModel> modelHashtable;
 	
-	public MemoryActionListener(MemoryModel model) {
-		this.model = model;
-		// TODO Auto-generated constructor stub
+	public MemoryActionListener(BaseModel model) {
+		modelHashtable = new Hashtable<Integer, BaseModel>();
+		this.addModel(model);
 	}
+	
+	public void addModel(BaseModel model) {
+		Integer key = model.hashCode();
+		modelHashtable.put(key, model);
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-
 		//get hashCode of the pressed card and call action on it:
 		int hashCode = ((BaseCard)e.getSource()).hashCode();
-		model.action(hashCode);
+		
+		Enumeration<Integer> enuma = modelHashtable.keys();
+	    while (enuma.hasMoreElements()) {
+	         int key = enuma.nextElement();
+	         ((BaseModel)modelHashtable.get(key)).action(hashCode);
+	    }
 	}
 }
