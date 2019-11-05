6c92e8ca00a276bec90a86181f054980999cc309
melanie-fuchs <mele.fuchs@gmx.ch>
2019-11-05 19:51:00 +0100

    creating PhotoPanel

--- src/gui/PhotoPanel.java
+++ src/gui/PhotoPanel.java
@@ -3,6 +3,8 @@
  */
 package gui;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -23,6 +25,7 @@ public class PhotoPanel extends JPanel {
 	private int numberOfCards;
 	private int numberOfPhotos;
 	private Vector<JLabel> photoLabelVector;
+	private JPanel photoPanel, chooserPanel;
 
 	
 	public void setPhoto(int fieldNo, File photoFile) {
@@ -44,17 +47,60 @@ public class PhotoPanel extends JPanel {
 			photoLabelVector.add(new JLabel());
 		}
 		
+		this.chooserPanel = new JPanel();
 		
 		this.numberOfCards = numberOfCards;
 		this.numberOfPhotos = numberOfCards / 2;
 		
-		this.setBorder(new EmptyBorder(10, 10, 10, 10));
 		
-		fillPanel();
+		createPhotoPanel();
+
+		
+		this.add(photoPanel, BorderLayout.CENTER);
+		this.add(chooserPanel, BorderLayout.SOUTH);
 	}
 	
 	
-	private void fillPanel() {
+	private JPanel createPhotoPanel() {
+		photoPanel = new JPanel();
+		photoPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+		photoPanel.setLayout(setGridLayout(numberOfPhotos));
+		loadImages();
+		
+		return photoPanel;
+	}
+	
+	
+	private JPanel createChooserPanel() {
+		chooserPanel = new JPanel();
+		chooserPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+		
+		return chooserPanel;
+		
+	}
+	/**
+	 * The method sets a GridLayout for the JPanel regarding the amount
+	 * of photos used in the game.
+	 * 
+	 * @param numberOfPhotos number of photos for the game
+	 */
+	private GridLayout setGridLayout(int numberOfPhotos) {
+		switch (numberOfPhotos) {
+		case 8:
+			return new GridLayout(4, 2, 7, 7);
+//			break;
+		case 10:
+			return new GridLayout(5, 2, 7, 7);
+//			break;
+		case 15:
+			return new GridLayout(5, 3, 7, 7);
+//			break;
+		default:
+			return null;
+		}
+	}
+	
+	private void loadImages() {
 		
 	}
 
