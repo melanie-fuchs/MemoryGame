c3bde78b2d266185e623a5bf42942126d4d38057
Melanie Fuchs <mele.fuchs@gmx.ch>
2019-11-05 11:09:32 +0100

    frame for chosen files preview

--- src/gui/PhotoChooser.java
+++ src/gui/PhotoChooser.java
@@ -10,14 +10,11 @@ public class PhotoChooser extends JFileChooser {
 	public PhotoChooser() {
 		super();
 		this.setLocation(600, 600);
-		
-		
+
 		selectedFiles = this.getSelectedFiles();
 		System.out.println("selected Files: ");
 		for(int i = 0; i < selectedFiles.length; i++) {
 			System.out.print(selectedFiles[i] + " ");
 		}
-	}
-	
-	
+	}	
 }
---
+++ src/gui/PhotoChooserFrame.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package gui;
+
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+/**
+ * @author yume
+ *
+ */
+public class PhotoChooserFrame extends JFrame {
+    private int gameSize;
+    private JPanel photoPanel, chooserPanel;
+    
+    public PhotoChooserFrame(int gameSize) {
+	this.gameSize = gameSize;
+	
+	this.setSize(720, 750);
+	this.setLocation(400, 0);
+	
+	createPhotoPanel(gameSize);
+	createChooserPanel();
+	
+	this.setVisible(true);
+    }
+
+    
+    private void createPhotoPanel(int gameSize) {
+	photoPanel = new JPanel();
+	photoPanel.setLayout(new GridLayout());
+	photoPanel.setBorder(new EmptyBorder(10, 10, 15, 10));
+	photoPanel.setLayout(new GridLayout(5, 6, 7, 7));
+    }
+    
+    private void createChooserPanel() {
+	chooserPanel = new JPanel();	
+    }
+}
