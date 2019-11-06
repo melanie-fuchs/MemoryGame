6c97f0b82128de9dc99a79a57c9d2910ab0afdb1
melanie-fuchs <mele.fuchs@gmx.ch>
2019-11-06 16:40:55 +0100

    enable startbutton after required amount of photos is loaded

--- src/gui/PhotoChooserFrame.java
+++ src/gui/PhotoChooserFrame.java
@@ -31,237 +31,234 @@ import javax.swing.filechooser.FileNameExtensionFilter;
  */
 public class PhotoChooserFrame extends JFrame {
 
-    private int numberOfCards, numberOfPhotos, thumbSize;
-    private Vector<JLabel> photoLabelVector;
-    private JPanel photoPanel, chooserPanel, chooserPanelTop, chooserPanelBottom;
-    private JButton jbtLoadImages, jbtStartGame;
-    private JFileChooser photoChooser;
-    private JFrame thisFrame = this;
-    private Vector<File> chosenFiles;
-    private JLabel jlMessage;
+	private int numberOfCards, numberOfPhotos, thumbSize;
+	private Vector<JLabel> photoLabelVector;
+	private JPanel photoPanel, chooserPanel, chooserPanelTop, chooserPanelBottom;
+	private JButton jbtLoadImages, jbtStartGame;
+	private JFileChooser photoChooser;
+	private JFrame thisFrame = this;
+	private Vector<File> chosenFiles;
+	private JLabel jlMessage;
 
-    public PhotoChooserFrame(int numberOfCards) {
-	this.numberOfCards = numberOfCards;
-	this.numberOfPhotos = numberOfCards / 2;
+	public PhotoChooserFrame(int numberOfCards) {
+		this.numberOfCards = numberOfCards;
+		this.numberOfPhotos = numberOfCards / 2;
 
-	this.photoLabelVector = new Vector<JLabel>();
-	for (int i = 0; i < numberOfPhotos; i++) {
-	    photoLabelVector.add(new JLabel());
+		this.photoLabelVector = new Vector<JLabel>();
+		for (int i = 0; i < numberOfPhotos; i++) {
+			photoLabelVector.add(new JLabel());
+		}
+
+		this.setSize(720, 500);
+		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		this.setLayout(new BorderLayout());
+
+		createPhotoPanel();
+		createChooserPanel();
+
+		this.add(photoPanel, BorderLayout.CENTER);
+		this.add(chooserPanel, BorderLayout.SOUTH);
+
+		this.setVisible(true);
 	}
 
-	this.setSize(720, 500);
-	this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-	this.setLayout(new BorderLayout());
-
-	createPhotoPanel();
-	createChooserPanel();
-
-	this.add(photoPanel, BorderLayout.CENTER);
-	this.add(chooserPanel, BorderLayout.SOUTH);
-
-	this.setVisible(true);
-    }
-
-    private JPanel createPhotoPanel() {
-	photoPanel = new JPanel();
-	photoPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
-	photoPanel.setLayout(setGridLayout(numberOfPhotos));
+	private JPanel createPhotoPanel() {
+		photoPanel = new JPanel();
+		photoPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+		photoPanel.setLayout(setGridLayout(numberOfPhotos));
 //		loadImages();
 
-	return photoPanel;
-    }
-
-    private void addFileToVector(File[] filesToAdd) {
-	if (filesToAdd != null && filesToAdd.length > 0) {
-	    for (File file : filesToAdd) {
-		if (!chosenFiles.contains(file)) {
-		    chosenFiles.add(file);
-		    System.out.println(file.hashCode() + " added");
-		}
-	    }
+		return photoPanel;
 	}
-    }
 
-    private JPanel createChooserPanel() {
-	chooserPanel = new JPanel();
-	chooserPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
-	chooserPanel.setLayout(new BorderLayout());
-
-	chooserPanel.add(getChooserPanelTop(), BorderLayout.CENTER);
-	chooserPanel.add(getChooserPanelBottom(), BorderLayout.SOUTH);
-	
-	return chooserPanel;
-    }
-
-    private JPanel getChooserPanelTop() {
-	chooserPanelTop = new JPanel();
-
-	// creating the button to load images (opens JFileChooser)
-	jbtLoadImages = new JButton("Load Images");
-	jbtLoadImages.addActionListener(new ActionListener() {
-	    @Override
-	    public void actionPerformed(ActionEvent e) {
-		if (photoChooser.showOpenDialog(thisFrame) == JFileChooser.APPROVE_OPTION) {
-		    addFileToVector(photoChooser.getSelectedFiles());
-		    // clearing selection
-		    photoChooser.setSelectedFile(new File(""));
-		    photoChooser.setSelectedFiles(new File[] { new File("") });
-		    
-		    handleLoadedFiles();
-		}
-	    }
-	});
-	chooserPanelTop.add(jbtLoadImages);
-	
-	
-	// creating the button to start the game. Will be set disabled while
-	// required numbers of photos is not loaded.
-	jbtStartGame = new JButton("Start Game");
-	jbtStartGame.addActionListener(new ActionListener() {
-	    @Override
-	    public void actionPerformed(ActionEvent e) {
-		// TODO Start the game
-	
-	    }
-	});
-	jbtStartGame.setEnabled(false); // TODO enable once all photos are set
-	chooserPanelTop.add(jbtStartGame);
-	
-	// creating and setting the JFileChooser
-	photoChooser = new JFileChooser();
-	FileNameExtensionFilter extensionFiler = new FileNameExtensionFilter("images", "jpg", "png", "gif", "jpeg");
-	photoChooser.setAcceptAllFileFilterUsed(false);
-	photoChooser.setMultiSelectionEnabled(true);
-	photoChooser.setFileFilter(extensionFiler);
-	chosenFiles = new Vector<File>();
-
-	return chooserPanelTop;
-    }
-    
-    private JPanel getChooserPanelBottom() {
-	chooserPanelBottom = new JPanel();
-
-	// creating message with instructions
-	jlMessage = new JLabel("Please choose " + numberOfPhotos
-		+ " unique photos form your file system");
-	chooserPanelBottom.add(jlMessage);
-	
-	return chooserPanelBottom;
-    }
-
-    public void setPhoto(int fieldNo, File photoFile) {
-	try {
-	    BufferedImage photo = ImageIO.read(photoFile);
-	    BufferedImage croppedPhoto = cropPhoto(photo);
-	    BufferedImage resizedPhoto = resizePhoto(croppedPhoto);
-	    JLabel tempLabel = photoLabelVector.elementAt(fieldNo);
-	    tempLabel.setIcon(new ImageIcon(resizedPhoto));
-	    tempLabel.setHorizontalAlignment(JLabel.CENTER);
-	    tempLabel.setVerticalAlignment(JLabel.CENTER);
-	    photoPanel.add(tempLabel);
-	} catch (IOException e) {
-	    System.out.println("PhotoChooserFrame.setPhoto(): loading image failed." + e.getMessage());
-	    e.printStackTrace();
-	}
-//	repaint();
-//	validate();
-    }
-    
-    private BufferedImage cropPhoto(BufferedImage originalPhoto) {
-	int photoWidth = originalPhoto.getWidth();
-	int photoHeight = originalPhoto.getHeight();
-	BufferedImage croppedImage;
-	if(photoHeight == photoWidth) {
-	    croppedImage = originalPhoto;	    
-	    return croppedImage;
-	} else {
-	    if(photoHeight > photoWidth) {
-		// cropping the photo, set square in the top half of the image
-		int cropY = (photoHeight - photoWidth) / 4;
-		croppedImage = originalPhoto.getSubimage(0, cropY, photoWidth, photoWidth);
-		return croppedImage;
-	    } else {
-			if (photoHeight < photoWidth) {
-			    // cropping the photo centered, full height
-			    int cropX = (photoWidth - photoHeight) / 2;
-			    croppedImage = originalPhoto.getSubimage(cropX, 0, photoHeight, photoHeight);
-			    return croppedImage;
+	private void addFileToVector(File[] filesToAdd) {
+		if (filesToAdd != null && filesToAdd.length > 0) {
+			for (File file : filesToAdd) {
+				if (!chosenFiles.contains(file)) {
+					chosenFiles.add(file);
+					System.out.println(file.hashCode() + " added");
+				}
 			}
-	    }
-	}
-	return null;
-    }
-
-    private BufferedImage resizePhoto(BufferedImage originalPhoto) {
-	if(originalPhoto != null) {
-	    BufferedImage resizedImage = new BufferedImage(thumbSize, thumbSize, originalPhoto.getType());
-	    Graphics2D graphic = resizedImage.createGraphics();
-	    graphic.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-	    graphic.drawImage(originalPhoto, 0, 0, thumbSize, thumbSize, 0, 0, originalPhoto.getWidth(),
-		    originalPhoto.getHeight(), null);
-	    graphic.dispose();
-	    return resizedImage;
-	}
-	return null;
-	
-    }
-    private void handleLoadedFiles() {
-	if (chosenFiles.size() == numberOfPhotos) {
-	    jlMessage.setText("Your cards are now set. You can start the game.");
-	    fillPhotoPanel();
-	} else {
-	    if (chosenFiles.size() > numberOfPhotos) {
-		jlMessage.setText("You chose too many photos. The first " + numberOfPhotos
-			+ " of the selected photos will be used."
-			+ " You can start the game now.");
-		// removing items so the vector contains only the required number of photos:
-		while(chosenFiles.size() > numberOfPhotos) {
-		    chosenFiles.remove(numberOfPhotos);
 		}
-		fillPhotoPanel();
-	    } else {
-		if (chosenFiles.size() < numberOfPhotos) {
-		    jlMessage.setText("Please choose another "
-		+ (numberOfPhotos - chosenFiles.size()) + " photos from your filesystem.");
+	}
+
+	private JPanel createChooserPanel() {
+		chooserPanel = new JPanel();
+		chooserPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+		chooserPanel.setLayout(new BorderLayout());
+
+		chooserPanel.add(getChooserPanelTop(), BorderLayout.CENTER);
+		chooserPanel.add(getChooserPanelBottom(), BorderLayout.SOUTH);
+
+		return chooserPanel;
+	}
+
+	private JPanel getChooserPanelTop() {
+		chooserPanelTop = new JPanel();
+
+		// creating the button to load images (opens JFileChooser)
+		jbtLoadImages = new JButton("Load Images");
+		jbtLoadImages.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (photoChooser.showOpenDialog(thisFrame) == JFileChooser.APPROVE_OPTION) {
+					addFileToVector(photoChooser.getSelectedFiles());
+					// clearing selection
+					photoChooser.setSelectedFile(new File(""));
+					photoChooser.setSelectedFiles(new File[] { new File("") });
+
+					handleLoadedFiles();
+				}
+			}
+		});
+		chooserPanelTop.add(jbtLoadImages);
+
+		// creating the button to start the game. Will be set disabled while
+		// required numbers of photos is not loaded.
+		jbtStartGame = new JButton("Start Game");
+		jbtStartGame.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Start the game
+
+			}
+		});
+		jbtStartGame.setEnabled(false); // TODO enable once all photos are set
+		chooserPanelTop.add(jbtStartGame);
+
+		// creating and setting the JFileChooser
+		photoChooser = new JFileChooser();
+		FileNameExtensionFilter extensionFiler = new FileNameExtensionFilter("images", "jpg", "png", "gif", "jpeg");
+		photoChooser.setAcceptAllFileFilterUsed(false);
+		photoChooser.setMultiSelectionEnabled(true);
+		photoChooser.setFileFilter(extensionFiler);
+		chosenFiles = new Vector<File>();
+
+		return chooserPanelTop;
+	}
+
+	private JPanel getChooserPanelBottom() {
+		chooserPanelBottom = new JPanel();
+
+		// creating message with instructions
+		jlMessage = new JLabel("Please choose " + numberOfPhotos + " unique photos form your file system");
+		chooserPanelBottom.add(jlMessage);
+
+		return chooserPanelBottom;
+	}
+
+	public void setPhoto(int fieldNo, File photoFile) {
+		try {
+			BufferedImage photo = ImageIO.read(photoFile);
+			BufferedImage croppedPhoto = cropPhoto(photo);
+			BufferedImage resizedPhoto = resizePhoto(croppedPhoto);
+			JLabel tempLabel = photoLabelVector.elementAt(fieldNo);
+			tempLabel.setIcon(new ImageIcon(resizedPhoto));
+			tempLabel.setHorizontalAlignment(JLabel.CENTER);
+			tempLabel.setVerticalAlignment(JLabel.CENTER);
+			photoPanel.add(tempLabel);
+		} catch (IOException e) {
+			System.out.println("PhotoChooserFrame.setPhoto(): loading image failed." + e.getMessage());
+			e.printStackTrace();
 		}
-	    }
 	}
-    }
-    
 
-    /**
-     * The method sets a GridLayout for the JPanel regarding the amount of photos
-     * used in the game.
-     * 
-     * @param numberOfPhotos number of photos for the game
-     */
-    private GridLayout setGridLayout(int numberOfPhotos) {
-	switch (numberOfPhotos) {
-	case 8:
-	    thumbSize = 150;
-	    return new GridLayout(2, 4, 7, 7);
-//			break;
-	case 10:
-	    thumbSize = 120;
-	    return new GridLayout(2, 5, 7, 7);
-//			break;
-	case 15:
-	    thumbSize = 120;
-	    return new GridLayout(3, 5, 7, 7);
-//			break;
-	default:
-	    return null;
+	private BufferedImage cropPhoto(BufferedImage originalPhoto) {
+		int photoWidth = originalPhoto.getWidth();
+		int photoHeight = originalPhoto.getHeight();
+		BufferedImage croppedImage;
+		if (photoHeight == photoWidth) {
+			croppedImage = originalPhoto;
+			return croppedImage;
+		} else {
+			if (photoHeight > photoWidth) {
+				// cropping the photo, set square in the top half of the image
+				int cropY = (photoHeight - photoWidth) / 4;
+				croppedImage = originalPhoto.getSubimage(0, cropY, photoWidth, photoWidth);
+				return croppedImage;
+			} else {
+				if (photoHeight < photoWidth) {
+					// cropping the photo centered, full height
+					int cropX = (photoWidth - photoHeight) / 2;
+					croppedImage = originalPhoto.getSubimage(cropX, 0, photoHeight, photoHeight);
+					return croppedImage;
+				}
+			}
+		}
+		return null;
 	}
-    }
 
-    private void fillPhotoPanel() {
-	for (int i = 0; i < numberOfPhotos; i++) {
-	    setPhoto(i, chosenFiles.elementAt(i));
+	private BufferedImage resizePhoto(BufferedImage originalPhoto) {
+		if (originalPhoto != null) {
+			BufferedImage resizedImage = new BufferedImage(thumbSize, thumbSize, originalPhoto.getType());
+			Graphics2D graphic = resizedImage.createGraphics();
+			graphic.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+			graphic.drawImage(originalPhoto, 0, 0, thumbSize, thumbSize, 0, 0, originalPhoto.getWidth(),
+					originalPhoto.getHeight(), null);
+			graphic.dispose();
+			return resizedImage;
+		}
+		return null;
+
 	}
-    }
 
-    public static void main(String[] args) {
-	new PhotoChooserFrame(30);
+	private void handleLoadedFiles() {
+		if (chosenFiles.size() == numberOfPhotos) {
+			jlMessage.setText("Your cards are now set. You can start the game.");
+			fillPhotoPanel();
+			jbtStartGame.setEnabled(true);
+		} else {
+			if (chosenFiles.size() > numberOfPhotos) {
+				jlMessage.setText("You chose too many photos. The first " + numberOfPhotos
+						+ " of the selected photos will be used." + " You can start the game now.");
+				// removing items so the vector contains only the required number of photos:
+				while (chosenFiles.size() > numberOfPhotos) {
+					chosenFiles.remove(numberOfPhotos);
+				}
+				fillPhotoPanel();
+				jbtStartGame.setEnabled(true);
+			} else {
+				if (chosenFiles.size() < numberOfPhotos) {
+					jlMessage.setText("Please choose another " + (numberOfPhotos - chosenFiles.size())
+							+ " photos from your filesystem.");
+				}
+			}
+		}
+	}
 
-    }
+	/**
+	 * The method sets a GridLayout for the JPanel regarding the amount of photos
+	 * used in the game.
+	 * 
+	 * @param numberOfPhotos number of photos for the game
+	 */
+	private GridLayout setGridLayout(int numberOfPhotos) {
+		switch (numberOfPhotos) {
+		case 8:
+			thumbSize = 150;
+			return new GridLayout(2, 4, 7, 7);
+//			break;
+		case 10:
+			thumbSize = 120;
+			return new GridLayout(2, 5, 7, 7);
+//			break;
+		case 15:
+			thumbSize = 120;
+			return new GridLayout(3, 5, 7, 7);
+//			break;
+		default:
+			return null;
+		}
+	}
+
+	private void fillPhotoPanel() {
+		for (int i = 0; i < numberOfPhotos; i++) {
+			setPhoto(i, chosenFiles.elementAt(i));
+		}
+	}
+
+	public static void main(String[] args) {
+		new PhotoChooserFrame(30);
+
+	}
 }
