ad9a1449f573e457f16e0bd6a93a3499df6d298a
melanie-fuchs <mele.fuchs@gmx.ch>
2019-11-06 17:15:42 +0100

    make pairs of colors

--- src/gui/StartMemory.java
+++ src/gui/StartMemory.java
@@ -67,6 +67,7 @@ public class StartMemory {
 	private void generateImageCards() {
 		for(int i = 0; i < (gameSize / 2); i++) {
 			cards.add(new CardImage(imageVector.elementAt(i), colorBack, i));
+			cards.add(new CardImage(imageVector.elementAt(i), colorBack, i));
 		}
 	}
 	
