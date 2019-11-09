163895c7fa43a8dd63a5555701a70fdc4aa64241
melanie-fuchs <mele.fuchs@gmx.ch>
2019-11-09 19:32:05 +0100

    javadoc + cleaning code

--- src/gui/MemoryCardPanel.java
+++ src/gui/MemoryCardPanel.java
@@ -16,50 +16,73 @@ import javax.swing.border.EmptyBorder;
  *
  */
 public class MemoryCardPanel extends JPanel {
-	
-	private int memorySize;		// 16, 20 or 30 cards possible
-	private void setMemorySize(int memorySize) {
-		this.memorySize = memorySize;
-	}
+	private static final long serialVersionUID = -7225763224244935931L;
+
+	/**
+	 * int-value that represents the number of cards in the game
+	 */
+	private int memorySize; // 16, 20 or 30 cards possible
+
+	/**
+	 * The method returns the number of cards in the game
+	 * 
+	 * @return int-value that represents the number of cards in the game
+	 */
 	public int getMemorySize() {
 		return memorySize;
 	}
-	
+
+	/**
+	 * A <code>Vector</code>-object that contains memory-cards of type
+	 * <code>BaseCard</code>
+	 */
 	private Vector<BaseCard> cards;
-	private void setCards(Vector<BaseCard> cards) {
-		this.cards = cards;
-	}
+
+	/**
+	 * The method returns the <code>Vector</code>-object 'cards'
+	 * 
+	 * @return <code>Vector</code> that contains memory-cards
+	 */
 	public Vector<BaseCard> getCards() {
 		return cards;
 	}
-	
-	private MemoryStatsPanel foreignPanel;
-	
-	private MemoryModel model = new MemoryModel();
-	
+
 	/**
-	 * Constructor that takes an int-value that represents the gamesize
-	 * (number of cards in the game), a Vector of memory-cards and a MemoryStatsPanel.
-	 * This MemoryStatsPanel will be used to connect the class with the foreign Panel
-	 * to display numbers.
+	 * A reference to another <code>JPanel</code> to update it.
+	 */
+	private MemoryStatsPanel foreignPanel;
+
+	/**
+	 * instance of class <code>MemoryModel</code>
+	 */
+	private MemoryModel model = new MemoryModel();
+
+	/**
+	 * Constructor that takes an int-value that represents the gamesize (number of
+	 * cards in the game), a Vector of memory-cards and a MemoryStatsPanel. This
+	 * MemoryStatsPanel will be used to connect the class with the foreign Panel to
+	 * display numbers.
 	 * 
-	 * @param memorySize
-	 * @param cards
+	 * @param memorySize   int-value that represents the total number of cards in
+	 *                     the game
+	 * @param cards        a <code>Vector</code>-object that contains memory-cards
+	 * @param foreignPanel a <code>MemoryStatsPanel</code> to update it's components
+	 *                     from outside the class
 	 */
 	public MemoryCardPanel(int memorySize, Vector<BaseCard> cards, MemoryStatsPanel foreignPanel) {
 		this.setBorder(new EmptyBorder(10, 10, 15, 10));
 		this.foreignPanel = foreignPanel;
-		setMemorySize(memorySize);
-		setCards(cards);
-		setGridLayout(memorySize);
-		fillPanel(cards);
+		this.memorySize = memorySize;
+		this.cards = cards;
+		this.setGridLayout(memorySize);
+		this.fillPanel(cards);
 	}
-	
+
 	/**
-	 * The method sets a GridLayout for the JPanel regarding the amount
-	 * of cards used in the game.
+	 * The method sets a GridLayout for the JPanel regarding the amount of cards
+	 * used in the game.
 	 * 
-	 * @param memorySize number of cards for the game
+	 * @param memorySize total number of cards for the game
 	 */
 	private void setGridLayout(int memorySize) {
 		switch (memorySize) {
@@ -67,27 +90,27 @@ public class MemoryCardPanel extends JPanel {
 			this.setLayout(new GridLayout(4, 4, 7, 7));
 			break;
 		case 20:
-			this.setLayout(new GridLayout(4, 5, 7 ,7));
+			this.setLayout(new GridLayout(4, 5, 7, 7));
 			break;
 		case 30:
 			this.setLayout(new GridLayout(5, 6, 7, 7));
 			break;
 		}
 	}
-	
+
 	/**
-	 * Method that shuffles the cards of the Vector and adds them to
-	 * the GridLayout.
+	 * Method that shuffles the cards of the <code>Vector</code> 'cards' and adds
+	 * them to the GridLayout.
 	 * 
-	 * @param cards
+	 * @param cards <code>Vector</code>-object that contains memory-cards
 	 */
 	private void fillPanel(Vector<BaseCard> cards) {
-		Collections.shuffle(cards);	// shuffle cards in random order
+		Collections.shuffle(cards); // shuffle cards in random order
 		for (int i = 0; i < cards.size(); i++) {
 			BaseCard currentCard = cards.get(i);
 			currentCard.addActionListener(new MemoryActionListener(model, foreignPanel));
 			this.add(currentCard);
 			model.registerCard(currentCard);
 		}
-	}	
+	}
 }
