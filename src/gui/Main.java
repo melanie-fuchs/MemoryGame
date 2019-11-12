dbb8eba81ed0d7fb2b7e0ab0d207673863b90545
Melanie Fuchs <mele.fuchs@gmx.ch>
2019-11-12 15:15:43 +0100

    javadoc once again

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -8,61 +8,84 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * The class extends <code>JFrame</code> and displays buttons to choose a
+ * language.
+ * 
+ * @author yume
+ *
+ */
 public class Main extends JFrame {
-    private JPanel panel;
-    private JButton buttonEnglish, buttonGerman;
-    private JLabel labelMemory;
-    private JFrame thisFrame = this;
+	private static final long serialVersionUID = -702599035722119310L;
+	private JPanel panel;
+	private JButton buttonEnglish, buttonGerman;
+	private JLabel labelMemory;
+	private JFrame thisFrame = this;
+	
+	/**
+	 * The constructor sets the size and location of the window.
+	 */
+	public Main() {
+		this.setSize(200, 150);
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		this.setLocation(dim.width / 2 - this.getSize().width / 2, dim.height / 2 - this.getSize().height / 2);
+		this.setLayout(new BorderLayout());
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-    public Main() {
-        this.setSize(200, 150);
-        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-        this.setLocation(dim.width / 2 - this.getSize().width / 2, dim.height / 2 - this.getSize().height / 2);
-        this.setLayout(new BorderLayout());
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		labelMemory = new JLabel("Memory", SwingConstants.CENTER);
+		labelMemory.setFont(new FontTitle(30));
+		labelMemory.setBorder(new EmptyBorder(15, 10, 10, 10));
+		this.add(labelMemory, BorderLayout.NORTH);
 
+		this.add(createGUI(), BorderLayout.CENTER);
+		this.setVisible(true);
+	}
+	
+	/**
+	 * The method creates a <code>JPanel</code>, adds two buttons onto it and
+	 * returns it. If a button is pressed, it's <code>ActionListener</code> will set
+	 * the language by calling the static <code>setLanguage</code>-method of class
+	 * Messages and then starts the memory-game.
+	 * 
+	 * @return a <code>JPanel</code>-object with buttons on it
+	 */
+	private JPanel createGUI() {
+		panel = new JPanel();
 
-        labelMemory = new JLabel("Memory", SwingConstants.CENTER);
-        labelMemory.setFont(new FontTitle(30));
-        labelMemory.setBorder(new EmptyBorder(15, 10, 10, 10));
-        this.add(labelMemory, BorderLayout.NORTH);
+		buttonEnglish = new JButton();
+		buttonEnglish.setIcon(new ImageIcon("icons\\en.png"));
+		buttonEnglish.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Messages.setLanguage("english");
+				new Memory();
+				thisFrame.dispose();
+			}
+		});
 
-        this.add(createGUI(), BorderLayout.CENTER);
-        this.setVisible(true);
-    }
+		buttonGerman = new JButton();
+		buttonGerman.setIcon(new ImageIcon("icons\\de.png"));
+		buttonGerman.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Messages.setLanguage("german");
+				new Memory();
+				thisFrame.dispose();
+			}
+		});
 
-    private JPanel createGUI() {
-        panel = new JPanel();
+		panel.add(buttonEnglish);
+		panel.add(buttonGerman);
 
-        buttonEnglish = new JButton();
-        buttonEnglish.setIcon(new ImageIcon("icons\\en.png"));
-        buttonEnglish.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                Messages.setLanguage("english");
-                new Memory();
-                thisFrame.dispose();
-            }
-        });
+		return panel;
+	}
 
-        buttonGerman = new JButton();
-        buttonGerman.setIcon(new ImageIcon("icons\\de.png"));
-        buttonGerman.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                Messages.setLanguage("german");
-                new Memory();
-                thisFrame.dispose();
-            }
-        });
-
-        panel.add(buttonEnglish);
-        panel.add(buttonGerman);
-
-        return panel;
-    }
-
-    public static  void main(String[] args) {
-        new Main();
-    }
+	/**
+	 * The mathod stats the game
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new Main();
+	}
 }
